64a239d13049dae90e37de027e6890b7afea3325
edatrix <lespath@gmail.com>
2014-06-02 15:13:21 -0600

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Baby8Ball::Application.config.secret_key_base = 'ef037fff5fecc1c9519d80e5010a569bfbbad9b4ca7c52afda9daf7284cb218c1fc5e689ddd4246e35f7d37b16525631be949417cb00efdffdf513371aff2ce7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Baby8Ball::Application.config.session_store :cookie_store, key: '_baby_8_ball_session'
