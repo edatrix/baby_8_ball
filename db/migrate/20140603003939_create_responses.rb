9f66b7cedfed46100a9363817803eb1f16ade535
edatrix <lespath@gmail.com>
2014-06-02 18:51:44 -0600

    adds responses migration to db

--- db/migrate/20140603003939_create_responses.rb
+++ db/migrate/20140603003939_create_responses.rb
@@ -1,8 +1,8 @@
 class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
-      t.string :title,
-      t.string :body,
+      t.string :title
+      t.string :description
       t.timestamps
     end
   end
