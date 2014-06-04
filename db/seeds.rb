3e1c5bdd55268d79b5569e2f5cf581ece353e1a5
edatrix <lespath@gmail.com>
2014-06-04 17:23:50 -0600

    adds seed data to production and updates index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-responses = [ {name: "skeptical", description: "i'm not sure you really want to know the answer to this question.", image: ""},
-  {name: "absolutely", description: "without a doubt.", image: "mommy.jpg"},
-  {name: "no", description: "this is a joke, right?", image: ""},
-  {name: "poop", description: "hang on, i'm pooping.", image: ""},
-  {name: "bored", description: "please bore someone else with your dull question.", image: ""},
-  {name: "annoyed", description: "how many times do i have to say 'no'?", image: ""},
-  {name: "lucky", description: "only if you're lucky.", image: ""},
+responses = [ {title: "skeptical", description: "i'm not sure you really want to know the answer to this question.", image: ""},
+  {title: "absolutely", description: "without a doubt.", image: "mommy.jpg"},
+  {title: "no", description: "this is a joke, right?", image: ""},
+  {title: "poop", description: "hang on, i'm pooping.", image: ""},
+  {title: "bored", description: "please bore someone else with your dull question.", image: ""},
+  {title: "annoyed", description: "how many times do i have to say 'no'?", image: ""},
+  {title: "lucky", description: "only if you're lucky.", image: ""},
    ]
 
-# responses.each do |response|
-#   Response.find_or_create_by_description(response)
-# end
+responses.each do |response|
+  Response.find_or_create_by_description!(response)
+end
