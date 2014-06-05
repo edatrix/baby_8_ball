92258f157c9a1b7c4e8620f71f91f362758020d6
edatrix <lespath@gmail.com>
2014-06-04 18:31:20 -0600

    adds paperclip

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,8 @@
 class Response < ActiveRecord::Base
   # validates_confirmation_of :title
+  has_attached_file :image, :stypes => { :medium => "300x300", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"}
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachmet_presence :image
 
   def self.random_response
     Response.all.sample
