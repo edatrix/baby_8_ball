b2e86abaed9196f749796d346fccea9c6bf8dd85
edatrix <lespath@gmail.com>
2014-06-05 21:29:44 -0600

    resizes photos and adds link to refresh on home

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-responses = [ {title: "i_cannot", description: "i can't even.", image: "chin_hand.png"},
-  {title: "absolutely", description: "without a doubt.", image: "mommy.png"},
+responses = [ {title: "i_cannot", description: "i can't even.", image: "mommy.png"},
+  {title: "absolutely", description: "without a doubt.", image: "chin_hand.png"},
   {title: "no", description: "this is a joke, right?", image: "furrowed.png"},
   {title: "poop", description: "hang on, i'm pooping.", image: "gross.png"},
   {title: "bored", description: "please bore someone else with your dull question.", image: "yawn.png"},
@@ -9,14 +9,14 @@ responses = [ {title: "i_cannot", description: "i can't even.", image: "chin_han
   {title: "for_sure", description: "abso-freakin-lutely!", image: "oooooh.png"},
   {title: "meh", description: "meh", image: "burrito.png"},
   {title: "cryptic", description: "the answer you seek is within.", image: "sleeping.png"},
-  {title: "maybe", description: "i've never been surer of anything.", image: "talking.png"},
-  {title: "not_good", description: "i'd tell you, but then i'd have to kill you.", image: "crying.png"},
+  {title: "maybe", description: "yeah, totally!", image: "talking.png"},
+  {title: "not_good", description: "whyyyy are you talking?", image: "crying.png"},
   {title: "possible", description: "anything is possible!", image: "red_suit.png"},
   {title: "no_chance", description: "not a chance", image: "sleepy.png"},
   {title: "later", description: "the answer is yours for $10.", image: "smirk.png"},
   {title: "waaah", description: "i can't take another question!", image: "waaah.png"},
   {title: "yes/no", description: "yes, no, whatever validation you need to feel good.", image: "blah.png"},
-  {title: "seriously", description: "seriously?", image: "not_amused"}
+  {title: "seriously", description: "seriously?", image: "not_amused.png"}
    ]
 
 responses.each do |response|
